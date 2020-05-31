08006a07ba84a6a8678658e3c63a9fdb71c834fc
Joe Pollock <yoeyyutch@hotmail.com>
2020-05-31 10:46:38 -0500

    fixed the directory error

--- SaviorSwingStats/Plugin.cs
+++ SaviorSwingStats/Plugin.cs
@@ -14,7 +14,9 @@ namespace SaviorSwingStats
     {
         internal static string Name => "SaviorSwingStats";
 
-        readonly string directory = @"C:\Program Files\Oculus\Software\Software\hyperbolic-magnetism-beat-saber\UserData\SaviorSwingStats\";
+        readonly string directory = @"C:\Program Files\Oculus\Software\Software\hyperbolic-magnetism-beat-saber\UserData\SaviorSwingStats";
+
+
 
         // private LevelData levelData;
         private StatKeeper statKeeper;
@@ -23,6 +25,7 @@ namespace SaviorSwingStats
         [Init]
         public void Init(IPALogger logger)
         {
+            Directory.CreateDirectory(@"C:\Program Files\Oculus\Software\Software\hyperbolic-magnetism-beat-saber\UserData\SaviorSwingStats");
             Logger.log = logger;
         }
 
@@ -58,7 +61,7 @@ namespace SaviorSwingStats
             {
                 List<string> songStatList = statKeeper.GetSongStatsheet();
                 //string songStats = statKeeper.GetSongStats();
-                string path = directory + "" + statKeeper.songName + "_" + statKeeper.songDifficulty + ".csv";
+                string path = directory + @"\" + statKeeper.songName + "_" + statKeeper.songDifficulty + ".txt";
                 SaveSongStats(path, songStatList);
                 statKeeper.ClearStats();
                 statKeeper = null;
@@ -80,13 +83,26 @@ namespace SaviorSwingStats
         //}
         private void SaveSongStats(string path, List<string> statlist)
         {
-            using (StreamWriter sw = File.AppendText(path))
+
+            string timestamp = DateTime.Now.ToString();
+
+            // This text is added only once to the file.
+            if (!File.Exists(path))
             {
-                sw.WriteLine("****** Song completed at " + DateTime.Now);
+                // Create a file to write to.
+                string createText = "Note ID,Note hit,Note Time, Direction, Lane, Level, Miss Distance, Time Deviation, Combo, Before Cut Rating, After CutRating" + Environment.NewLine;
+                File.WriteAllText(path, createText);
             }
+
+            //using (StreamWriter sw = File.AppendText(path))
+            //{
+            //    sw.WriteLine("****** Song completed at " + DateTime.Now);
+            //}
+
+            File.AppendAllText(path, timestamp);
             File.AppendAllLines(path, statlist);
             Logger.log.Info("5 Song stats saved to file.");
-        
+
         }
     }
 }
